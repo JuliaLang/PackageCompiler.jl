06dcae5f4413114f476c8311686e9774c967c1b8
Jameson Nash <vtjnash@gmail.com>
2015-12-10 18:02:49 -0500

    expand readme as a markdown document and add inline comments on example source files

--- program.c
+++ program.c
@@ -1,24 +1,31 @@
 // This file is a part of Julia. License is MIT: http://julialang.org/license
 
+// Standard headers
 #include <string.h>
 #include <stdint.h>
 
+// Julia headers (for initialization and gc commands)
 #include "uv.h"
 #include "julia.h"
 
+// Declare C prototype of a function defined in Julia
 extern intptr_t (*julia_hello)(char*, intptr_t);
 
 int main(int argc, char *argv[])
 {
+    intptr_t v;
+
+    // Initialize Julia
     uv_setup_args(argc, argv); // no-op on Windows
     libsupport_init();
     jl_options.compile_enabled = JL_OPTIONS_COMPILE_OFF;
     jl_options.image_file = "sys-plus.dylib";
     julia_init(JL_IMAGE_CWD);
 
-    julia_hello(argv[argc - 1], 42);
+    // Do some work
+    v = julia_hello(argv[argc - 1], 42);
 
+    // Cleanup and graceful exit
     jl_atexit_hook(0);
     return 0;
 }
-
--- program2.c
+++ program2.c
@@ -1,21 +1,27 @@
 // This file is a part of Julia. License is MIT: http://julialang.org/license
 
+// Standard headers
 #include <string.h>
 #include <stdint.h>
 
+// Julia headers (for initialization and gc commands)
 #include "uv.h"
 #include "julia.h"
 
-extern void (*libcas_tech_main)(jl_array_t*);
+// Declare C prototype of a function defined in Julia
+extern int (*c_decl_name)(jl_array_t*);
 
+// main function (windows UTF16 -> UTF8 argument conversion code copied from julia's ui/repl.c)
 #ifndef _OS_WINDOWS_
 int main(int argc, char *argv[])
 {
+    int retcode;
     int i;
     uv_setup_args(argc, argv); // no-op on Windows
 #else
 int wmain(int argc, wchar_t *wargv[], wchar_t *envp[])
 {
+    int retcode;
     int i;
     char **argv;
     for (i = 0; i < argc; i++) { // convert the command line to UTF8
@@ -31,10 +37,10 @@ int wmain(int argc, wchar_t *wargv[], wchar_t *envp[])
     // initialization
     libsupport_init();
     jl_options.compile_enabled = JL_OPTIONS_COMPILE_OFF;
-    jl_options.image_file = argv[0]; //"castech_legacy.cbe.dylib";
+    jl_options.image_file = argv[0];
     julia_init(JL_IMAGE_CWD);
 
-    // build arguments
+    // build arguments array: `UTF8String[ bytestring(argv[i]) for i in 1:argc ]`
     jl_array_t *ARGS = jl_alloc_array_1d(jl_apply_array_type(jl_utf8_string_type, 1), 0);
     JL_GC_PUSH1(&ARGS);
     jl_array_grow_end(ARGS, argc - 1);
@@ -43,9 +49,11 @@ int wmain(int argc, wchar_t *wargv[], wchar_t *envp[])
         jl_set_typeof(s, jl_utf8_string_type);
         jl_arrayset(ARGS, s, i - 1);
     }
-    libcas_tech_main(ARGS);
+    // call the work function, and get back a value
+    retcode = c_decl_name(ARGS);
     JL_GC_POP();
 
-    jl_atexit_hook(0);
-    return 0;
+    // Cleanup and gracefully exit
+    jl_atexit_hook(retcode);
+    return retcode;
 }
