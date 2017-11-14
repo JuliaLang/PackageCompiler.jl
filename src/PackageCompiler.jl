8516235d577c7ce9a9f9a7ecc64c576a51ac0b38
SimonDanisch <sdanisch@gmail.com>
2017-11-14 17:57:53 +0100

    initial commit

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -1,5 +1,56 @@
 module PackageCompiler
 
-# package code goes here
+using SnoopCompile
+
+function snoop(path, compilationfile)
+    mktempdir() do tmpfolder
+        csv = joinpath(tmpfolder, "precompile.csv")
+        # Snoop compiler can't handle the path as a variable, so we just create a file
+        open(joinpath(tmpfolder, "snoopy.jl"), "w") do io
+            println(io, "mod = include(\"$path\")")
+            println(io, "mod.run()")
+            println(io, "mod")
+        end
+        cd(tmpfolder)
+        mod = SnoopCompile.@snoop csv begin
+            include("snoopy.jl")
+        end
+        println(mod)
+        data = SnoopCompile.read(csv)
+        pc = SnoopCompile.parcel(reverse!(data[2]))
+        mod = include(path) # just get the module name
+        modsym = Symbol(mod)
+        modstr = String(modsym)
+        delims = r"([\{\} \n\(\),])_([\{\} \n\(\),])"
+        open(compilationfile, "w") do io
+            for (k, v) in pc
+                k in (:unknown, modsym) && continue
+                println(io, "using $k")
+            end
+            for (k, v) in pc
+                k == modsym && continue
+                for ln in v
+                    if !contains(ln, modstr)
+                        # replace `_` for free parameters, which print out a warning otherwise
+                        ln = replace(ln, delims, s"\1XXX\2")
+                        println(io, ln)
+                    end
+                end
+            end
+        end
+    end
+end
+
+include(joinpath(JULIA_HOME, "..", "..", "contrib", "build_sysimg.jl"))
+
+function compile(file; force = false)
+    build_sysimg(default_sysimg_path(), "native", file, force = force)
+end
+
+function revert(file; force = false)
+    build_sysimg(force = true)
+end
+
+
 
 end # module
