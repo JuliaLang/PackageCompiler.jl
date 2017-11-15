5a1394f8d965f2503ea0c2e10012be4d9e93b985
SimonDanisch <sdanisch@gmail.com>
2017-11-15 20:25:51 +0100

    fix problems with invalid precompile statemtents

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -6,16 +6,16 @@ function snoop(path, compilationfile)
     mktempdir() do tmpfolder
         csv = joinpath(tmpfolder, "precompile.csv")
         # Snoop compiler can't handle the path as a variable, so we just create a file
+
         open(joinpath(tmpfolder, "snoopy.jl"), "w") do io
-            println(io, "mod = include(\"$path\")")
+            println(io, "mod = include(\"$(escape_string(path))\")")
             println(io, "mod.run()")
             println(io, "mod")
         end
         cd(tmpfolder)
-        mod = SnoopCompile.@snoop csv begin
+        SnoopCompile.@snoop csv begin
             include("snoopy.jl")
         end
-        println(mod)
         data = SnoopCompile.read(csv)
         pc = SnoopCompile.parcel(reverse!(data[2]))
         mod = include(path) # just get the module name
@@ -26,14 +26,22 @@ function snoop(path, compilationfile)
             for (k, v) in pc
                 k in (:unknown, modsym) && continue
                 println(io, "using $k")
+                eval(:(using $k))
             end
             for (k, v) in pc
                 k == modsym && continue
+                println(k)
                 for ln in v
                     if !contains(ln, modstr)
                         # replace `_` for free parameters, which print out a warning otherwise
                         ln = replace(ln, delims, s"\1XXX\2")
-                        println(io, ln)
+                        # only print out valid lines
+                        try
+                            eval(parse(ln))
+                            println(io, ln)
+                        catch e
+                            warn(e)
+                        end
                     end
                 end
             end
@@ -41,7 +49,8 @@ function snoop(path, compilationfile)
     end
 end
 
-include(joinpath(JULIA_HOME, "..", "..", "contrib", "build_sysimg.jl"))
+
+include(joinpath(JULIA_HOME, Base.DATAROOTDIR,"julia", "build_sysimg.jl"))
 
 function compile(file; force = false)
     build_sysimg(default_sysimg_path(), "native", file, force = force)
