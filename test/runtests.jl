85df43bd9247e0edd9653b6a5edb74a423182008
Simon <sdanisch@gmail.com>
2017-12-02 00:16:06 +0100

    address #3

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using PackageCompiler
 using Base.Test
 
-# write your own tests here
 # If this works without error we should be in pretty good shape!
-PackageCompiler.compile_package("Matcha", false)
+# This command will use the runtest.jl of Matcha to find out what functions to precompile!
+PackageCompiler.compile_package("Matcha", false) # false to not force overwriting julia's current system image
 # TODO test revert - I suppose i wouldn't have enough rights on travis to move around dll's?
