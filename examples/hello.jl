c1044f1a7da04a0ede34a699d027201ede6b00ca
SimonDanisch <sdanisch@gmail.com>
2018-02-18 14:20:03 +0100

    go a bit lighter on dependencies

--- examples/hello.jl
+++ examples/hello.jl
@@ -1,13 +1,11 @@
 module Hello
 
 using UnicodePlots
-using Distributions
-f() = rand(Normal())
 
 Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
     println("hello, world")
-    @show f()
-    println(lineplot(1:10, (1:10).^2))
+    @show sin(0.0)
+    println(lineplot(1:10, sin.(linspace(0, 4, 10))))
     return 0
 end
 
