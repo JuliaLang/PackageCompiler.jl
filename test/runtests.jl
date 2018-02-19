fe00c802add5a36840df8a5fc2dd1c51b9aee10e
SimonDanisch <sdanisch@gmail.com>
2018-02-19 20:06:03 +0100

    fix test error on metadata

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,14 @@ PackageCompiler.compile_package("Matcha", "UnicodeFun", force = false, reuse = f
 # build again, with resuing the snoop file
 img_file = PackageCompiler.compile_package("Matcha", "UnicodeFun", force = false, reuse = true)
 # TODO test revert - I suppose i wouldn't have enough rights on travis to move around dll's?
-
+julia = Base.julia_cmd().exec[1]
 @testset "basic tests" begin
     @test isfile(img_file)
     userimg = PackageCompiler.sysimg_folder("precompile.jl")
     @test isfile(userimg)
     # Make sure we actually snooped stuff
     @test length(readlines(userimg)) > 700
-    @test success(`julia -J $(img_file)`)
+    @test success(`$julia -J $(img_file)`)
     mktempdir() do dir
         sysfile = joinpath(dir, "sys")
         PackageCompiler.compile_system_image(sysfile, "native")
@@ -29,7 +29,6 @@ end
         juliac = joinpath(@__DIR__, "..", "juliac.jl")
         jlfile = joinpath(@__DIR__, "..", "examples", "hello.jl")
         cfile = joinpath(@__DIR__, "..", "examples", "program.c")
-        julia = Base.julia_cmd()
         @test success(`$julia $juliac -vosej $jlfile $cfile $build`)
         @test isfile(joinpath(build, "hello.$(Libdl.dlext)"))
         @test isfile(joinpath(build, "hello$(executable_ext())"))
