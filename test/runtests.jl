a6f5023bc788c94cf02e63cf9956406178f3c82f
Nathan Daly <nhdaly@gmail.com>
2018-07-19 18:32:55 -0400

    Fix tests for julia v0.7 for static_julia

--- test/runtests.jl
+++ test/runtests.jl
@@ -72,16 +72,16 @@ end
         )
         # Check that the output from the program is as expected:
         exe = joinpath(builddir, outname*executable_ext)
-        output = readstring(`$exe a b c`)
+        output = read(`$exe a b c`, String)
         println(output)
-        @test all(s->contains(output, s), [
+        @test all(s->occursin(s, output), [
                 "@__FILE__: " * argsjlfile
                 "PROGRAM_FILE: " * exe
-                """argv: String["a", "b", "c"]"""
-                """ARGS: String["a", "b", "c"]"""
-                """Base.ARGS: String["a", "b", "c"]"""
-                # This one is a regex because it's difficult to correctly match
-                # both the unix and windows outputs.
+                # These are regexes b/c output is different on v0.6 and v0.7.
+                # All we really want to test is that it has the right args.
+                r"argv: .*[\"a\", \"b\", \"c\"]"
+                r"ARGS: .*[\"a\", \"b\", \"c\"]"
+                r"Base.ARGS: .*[\"a\", \"b\", \"c\"]"
                 r"Core.ARGS: .*[\".*builddir.*outname.*\", \"a\", \"b\", \"c\"]"
             ]
         )
@@ -100,9 +100,9 @@ end
         @testset "--cc-flags" begin
             # Try passing `--help` to $cc. This should work for any system compiler.
             # Then grep the output for "-g", which should be present on any system.
-            @test contains(readstring(`$julia $juliac -se --cc-flags="--help" $jlfile $cfile --builddir $builddir`), "-g")
+            @test occursin("-g", read(`$julia $juliac -se --cc-flags="--help" $jlfile $cfile --builddir $builddir`, String))
             # Just as a control, make sure that without passing '--help', we don't see "-g"
-            @test !contains(readstring(`$julia $juliac -se $jlfile $cfile --builddir $builddir`), "-g")
+            @test !occursin("-g", read(`$julia $juliac -se $jlfile $cfile --builddir $builddir`, String))
         end
     end
 end
