6c5fa31d7c379f7b6a4fc044a1f776d0706b70cb
SimonDanisch <sdanisch@gmail.com>
2019-03-25 16:34:21 +0100

    globally blacklist + dont init by default

--- src/snooping.jl
+++ src/snooping.jl
@@ -1,14 +1,37 @@
 using Pkg, Serialization
 
 
-const known_uninitializable_libs = [
-    :Distributed, :SpecialFunctions
-]
+const packages_needing_initialization = Symbol[]
+
+"""
+    init_package!(packages::Symbol...)
+Globally add `packages` to the list of packages that need their `__init__`
+method called when ahead of time compiling.
+
+(adds it only for the current session).
+"""
+function init_package!(packages::Symbol...)
+    push!(packages_needing_initialization, packages...)
+end
+
 const known_blacklisted_packages = Symbol[]
 
+# Some packages directly depend on Homebrew/QuartzImageIO, even on non apple systems,
+# Which yields an annoying warning compiled directly into the system image.
 if !Sys.isapple()
-    push!(known_blacklisted_packages, :QuartzImageIO)
+    push!(known_blacklisted_packages, :QuartzImageIO, :Homebrew)
 end
+
+
+"""
+    blacklist!(packages::Symbol...)
+Globally blacklists a package that is known to not ahead of time compile.
+(Blacklists only for the current session).
+"""
+function blacklist!(packages::Symbol...)
+    push!(known_blacklisted_packages, packages...)
+end
+
 
 function snoop(snoopfile::String, output_io::IO; verbose = false)
     # make sure our variables don't conflict with any precompile statements
@@ -39,6 +62,9 @@ function snoop(snoopfile::String, output_io::IO; verbose = false)
         # replace ##symbols
         #line = replace(line, r"(?!\")(##.*?#\d+?)(?!\")" => s"eval(Symbol(\"\1\"))")
 
+        # replace character literals in type paramters, which can't be constructed from their data
+        line = replace(line, r"Char\((0x[\da-f]{8})\)" => s"reinterpret(Char, \1)")
+
         try
             expr = Meta.parse(line, raise = true)
             if expr.head != :incomplete
@@ -64,8 +90,6 @@ end
 
 function snoop_packages(
         packages::Vector{String}, file::String;
-        blacklist::Vector{Symbol} = known_blacklisted_packages,
-        init_blacklist::Vector{Symbol} = known_uninitializable_libs,
         install::Bool = false, verbose = false
     )
     pkgs = PackageSpec.(packages)
@@ -73,9 +97,10 @@ function snoop_packages(
     packages = resolve_full_dependencies(pkgs, install = install)
 
     # remove blacklisted packages from full list of packages
-    package_names = setdiff(getfield.(packages, :name), string.(blacklist))
+    package_names = setdiff(getfield.(packages, :name), string.(known_blacklisted_packages))
 
-    inits = setdiff(package_names, string.(init_blacklist))
+    inits = string.(packages_needing_initialization)
+    println(inits)
     usings = join("import " .* package_names, "\n")
     inits = join("    " .* inits, ",\n")
     open(file, "w") do io
