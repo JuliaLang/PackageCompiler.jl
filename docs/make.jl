48c6aebf207827925e356f994013a1a96352291e
Kristoffer Carlsson <kcarlsson89@gmail.com>
2019-12-12 12:05:30 +0100

    Developer docs (#57)
    * import the blog posts
    
    * wip
    
    * move blogposts to docs
    
    * some tweaks

--- docs/make.jl
+++ docs/make.jl
@@ -6,17 +6,26 @@ makedocs(
         prettyurls = haskey(ENV, "HAS_JOSH_K_SEAL_OF_APPROVAL"),
     ),
     sitename = "PackageCompilerX",
-    pages = Any[
+    pages = [
         "Home" => "index.md",
+
         "Manual" => [
             "prereq.md"
             "sysimages.md"
             "apps.md"
         ],
 
-        "Examples" => Any[
+        "Examples" => [
             "examples/ohmyrepl.md",
         ],
+
+        "PackageCompilerX - the manual way" => [
+            "devdocs/intro.md",
+            "devdocs/sysimages_part_1.md",
+            "devdocs/binaries_part_2.md",
+            "devdocs/relocatable_part_3.md",
+        ],
+
         "References" => "refs.md",
     ]
 )
