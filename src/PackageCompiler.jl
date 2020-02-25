3061935c840a0dd76a138aa3aaf3555a69549f1c
Kristoffer Carlsson <kcarlsson89@gmail.com>
2020-02-25 19:03:17 +0100

    also abspath the precompile execution file (#353)

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -365,6 +365,7 @@ function create_sysimage(packages::Union{Symbol, Vector{Symbol}}=Symbol[];
                          base_sysimage::Union{Nothing, String}=nothing,
                          isapp::Bool=false)
     precompile_statements_file = abspath.(precompile_statements_file)
+    precompile_execution_file = abspath.(precompile_execution_file)
     if replace_default==true
         if sysimage_path !== nothing
             error("cannot specify `sysimage_path` when `replace_default` is `true`")
@@ -573,6 +574,7 @@ function create_app(package_dir::String,
                     force=false,
                     cpu_target::String=default_app_cpu_target())
     precompile_statements_file = abspath.(precompile_statements_file)
+    precompile_execution_file = abspath.(precompile_execution_file)
     package_dir = abspath(package_dir)
     ctx = create_pkg_context(package_dir)
     if isempty(ctx.env.manifest)
