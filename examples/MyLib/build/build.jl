91aa2a331e117bb7fb461d024e7b61afbc93fad4
Kevin Squire <kevin@secondspectrum.com>
2021-07-04 10:31:17 -0700

    Update MyLib example

--- examples/MyLib/build/build.jl
+++ examples/MyLib/build/build.jl
@@ -1,17 +1,16 @@
 using PackageCompiler
 
-target_dir = get(ENV, "OUTDIR", "./target")
+target_dir = get(ENV, "OUTDIR", "$(@__DIR__)/../../MyLibCompiled")
+target_dir = replace(target_dir, "\\"=>"/")       # Change Windows paths to use "/"
 
-# Change Windows paths to use "/"
-target_dir = replace(target_dir, "\\"=>"/")
+package_dir =
 
 println("Creating library in $target_dir")
-
 PackageCompiler.create_library(".", target_dir;
-                                lib_name="inc",
-                                precompile_execution_file=["build/generate_precompile.jl"],
-                                precompile_statements_file=["build/additional_precompile.jl"],
+                                lib_name="mylib",
+                                precompile_execution_file=["$(@__DIR__)/generate_precompile.jl"],
+                                precompile_statements_file=["$(@__DIR__)/additional_precompile.jl"],
                                 incremental=false,
                                 filter_stdlibs=true,
-                                header_files = ["./inc.h"],
+                                header_files = ["$(@__DIR__)/mylib.h"],
                             )
