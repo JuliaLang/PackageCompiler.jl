c227816dedb8b574da1b515cb5980b62bcaa0a3d
KristofferC <kcarlsson89@gmail.com>
2021-09-16 16:28:53 +0200

    hide the "devdocs" in the manual

--- docs/make.jl
+++ docs/make.jl
@@ -19,12 +19,14 @@ makedocs(
             "examples/plots.md",
         ],
 
+        #=
         "PackageCompiler - the manual way" => [
             "devdocs/intro.md",
             "devdocs/sysimages_part_1.md",
             "devdocs/binaries_part_2.md",
             "devdocs/relocatable_part_3.md",
         ],
+        =#
 
         "References" => "refs.md",
         "Upgrade notes" => "upgrade.md",
