8c1757dc3423dbf75c3ed682c3bcc16d3f060a39
Ian <i.r.butterworth@gmail.com>
2021-09-29 15:36:10 -0400

    better check

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -508,12 +508,11 @@ function create_sysimage(packages::Union{Symbol, Vector{String}, Vector{Symbol}}
     copy!(LOAD_PATH, [project])
     try
         Pkg.instantiate(ctx, verbose=true, allow_autoprecomp = false)
-        try
-            # try to disable warning about precompiling packages that are already loaded given work will be done in new
+        if :warn_loaded in Base.kwarg_decl(first(methods(Pkg.precompile, Tuple{Pkg.Types.Context})))
+            # if the kwarg is available, disable warning about precompiling packages that are already loaded given work will be done in new
             # julia sessions. The `warn_loaded` kwarg was introduced in https://github.com/JuliaLang/Pkg.jl/pull/2739
-            # so isn't present on all versions, so try and do standard precompilation if fails
             Pkg.precompile(ctx, warn_loaded = false)
-        catch
+        else
             Pkg.precompile(ctx)
         end
     finally
