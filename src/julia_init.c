10ffe6570d9597ebaebd689719869df97559134f
KristofferC <kcarlsson89@gmail.com>
2021-10-22 15:26:44 +0200

    format c files

--- src/julia_init.c
+++ src/julia_init.c
@@ -1,6 +1,6 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 #ifdef _MSC_VER
 JL_DLLEXPORT char *dirname(char *);
@@ -9,65 +9,59 @@ JL_DLLEXPORT char *dirname(char *);
 #endif
 
 // Julia headers (for initialization and gc commands)
-#include "uv.h"
 #include "julia.h"
 #include "julia_init.h"
+#include "uv.h"
 
-
-void setup_args(int argc, char **argv)
-{
+void setup_args(int argc, char **argv) {
     uv_setup_args(argc, argv);
     jl_parse_opts(&argc, &argv);
 }
 
-const char *get_sysimage_path(const char *libname)
-{
-    if (libname == NULL)
-    {
+const char *get_sysimage_path(const char *libname) {
+    if (libname == NULL) {
         jl_error("julia: Specify `libname` when requesting the sysimage path");
         exit(1);
     }
 
     void *handle = jl_load_dynamic_library(libname, JL_RTLD_DEFAULT, 0);
-    if (handle == NULL)
-    {
+    if (handle == NULL) {
         jl_errorf("julia: Failed to load library at %s", libname);
         exit(1);
     }
 
     const char *libpath = jl_pathname_for_handle(handle);
-    if (libpath == NULL)
-    {
-        jl_errorf("julia: Failed to retrieve path name for library at %s", libname);
+    if (libpath == NULL) {
+        jl_errorf("julia: Failed to retrieve path name for library at %s",
+                  libname);
         exit(1);
     }
 
     return libpath;
 }
 
-void set_depot_load_path(const char *root_dir)
-{
-    #ifdef _WIN32
-        char *julia_share_subdir = "\\share\\julia";
-    #else
-        char *julia_share_subdir = "/share/julia";
-    #endif
-    char *share_dir = calloc(sizeof(char), strlen(root_dir) + strlen(julia_share_subdir) + 1);
+void set_depot_load_path(const char *root_dir) {
+#ifdef _WIN32
+    char *julia_share_subdir = "\\share\\julia";
+#else
+    char *julia_share_subdir = "/share/julia";
+#endif
+    char *share_dir =
+        calloc(sizeof(char), strlen(root_dir) + strlen(julia_share_subdir) + 1);
     strcat(share_dir, root_dir);
     strcat(share_dir, julia_share_subdir);
 
-    #ifdef _WIN32
-        _putenv_s("JULIA_DEPOT_PATH", share_dir);
-        _putenv_s("JULIA_LOAD_PATH", share_dir);
-    #else
-        setenv("JULIA_DEPOT_PATH", share_dir, 1);
-        setenv("JULIA_LOAD_PATH", share_dir, 1);
-    #endif
+#ifdef _WIN32
+    _putenv_s("JULIA_DEPOT_PATH", share_dir);
+    _putenv_s("JULIA_LOAD_PATH", share_dir);
+#else
+    setenv("JULIA_DEPOT_PATH", share_dir, 1);
+    setenv("JULIA_LOAD_PATH", share_dir, 1);
+#endif
     free(share_dir);
 }
 
-void init_julia(int argc, char **argv)
-{
+void init_julia(int argc, char **argv) {
     setup_args(argc, argv);
 
     const char *sysimage_path = get_sysimage_path(JULIAC_PROGRAM_LIBNAME);
@@ -80,7 +74,4 @@ void init_julia(int argc, char **argv)
     julia_init(JL_IMAGE_CWD);
 }
 
-void shutdown_julia(int retcode)
-{
-    jl_atexit_hook(retcode);
-}
+void shutdown_julia(int retcode) { jl_atexit_hook(retcode); }
