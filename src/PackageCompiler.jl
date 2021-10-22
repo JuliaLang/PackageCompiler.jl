d34dd30f0abd5faf03badaa9867a308dfe5f45e1
KristofferC <kcarlsson89@gmail.com>
2021-10-22 13:58:54 +0200

    fix symlinking

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -881,10 +881,12 @@ function create_library(package_dir::String,
         soname)
 
     if version !== nothing && Sys.isunix()
-        base_file = get_sysimg_file(lib_name)
-        @debug "creating symlinks for $compat_file and $base_file"
-        symlink(sysimg_path, joinpath(lib_dir, compat_file))
-        symlink(sysimg_path, joinpath(lib_dir, base_file))
+        cd(dirname(sysimg_path)) do
+            base_file = get_sysimg_file(lib_name)
+            @debug "creating symlinks for $compat_file and $base_file"
+            symlink(sysimg_file, compat_file)
+            symlink(sysimg_file, base_file)
+        end
     end
 end
 
