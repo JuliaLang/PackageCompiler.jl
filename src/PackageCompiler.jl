e5a8410394a8054b91ea19fd80abb4d67d64442b
Alexander Seiler <seileralex@gmail.com>
2023-03-27 11:53:48 +0200

    Fix two typos (#797)
    Signed-off-by: Alexander Seiler <seileralex@gmail.com>

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -223,7 +223,7 @@ function create_fresh_base_sysimage(stdlibs::Vector{String}; cpu_target::String,
     compiler_source_path = joinpath(base_dir, "compiler", "compiler.jl")
 
     # we can't strip the IR from the base sysimg, so we filter out this flag
-    # also presuambly `--compile=all`` and maybe a few others we missed here...
+    # also presumably `--compile=all` and maybe a few others we missed here...
     sysimage_build_args_strs = map(p -> "$(p...)", values(sysimage_build_args))
     filter!(p -> !contains(p, "--compile") && p ∉ ("--strip-ir",), sysimage_build_args_strs)
     sysimage_build_args = Cmd(sysimage_build_args_strs)
