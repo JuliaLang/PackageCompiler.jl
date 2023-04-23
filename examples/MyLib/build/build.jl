696064c28c0a5fa82b43de95251a18bc3c3e5b2e
Rik Huijzer <t.h.huijzer@rug.nl>
2023-04-23 21:31:47 +0200

    Build and run `MyLib` (#732)
    Co-authored-by: Kristoffer Carlsson <kcarlsson89@gmail.com>

--- examples/MyLib/build/build.jl
+++ examples/MyLib/build/build.jl
@@ -1,6 +1,6 @@
 using PackageCompiler
 
-target_dir = get(ENV, "OUTDIR", "$(@__DIR__)/../../MyLibCompiled")
+target_dir = get(ENV, "OUTDIR", "$(@__DIR__)/../MyLibCompiled")
 target_dir = replace(target_dir, "\\"=>"/")       # Change Windows paths to use "/"
 
 println("Creating library in $target_dir")
@@ -10,5 +10,6 @@ PackageCompiler.create_library(".", target_dir;
                                 precompile_statements_file=["$(@__DIR__)/additional_precompile.jl"],
                                 incremental=false,
                                 filter_stdlibs=true,
+                                force=true, # Overwrite target_dir.
                                 header_files = ["$(@__DIR__)/mylib.h"],
                             )
