56a1b98db8249b38001b4e63ce8dd607a38b0def
ktdq <105746631+ktdq@users.noreply.github.com>
2023-10-22 02:51:12 -0400

    Thread parallel processing of precompile statements

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -339,7 +339,7 @@ function create_sysimg_object_file(object_file::String,
             precompile_files = String[
                 $(join(map(repr, precompile_files), "\n" * " " ^ 8))
             ]
-            for file in precompile_files, statement in eachline(file)
+            Threads.@threads for statement in collect(Iterators.flatten(eachline.(precompile_files)))
                 # println(statement)
                 # This is taken from https://github.com/JuliaLang/julia/blob/2c9e051c460dd9700e6814c8e49cc1f119ed8b41/contrib/generate_precompile.jl#L375-L393
                 ps = try
