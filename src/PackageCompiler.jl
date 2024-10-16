467b90b2d6c2489b475a288f04bb9c6a50947b8b
Gabriel Baraldi <baraldigabriel@gmail.com>
2024-10-15 21:47:00 -0300

    Change naming convention of object files to follow what Base Julia does (#930)
    * Change naming convention of object files to follow what Base Julia does
    
    * Add some comments linking to the PR and the bug report
    
    Thus, future readers of the source code will understand why we use this naming convention.
    
    [Applies suggestions from code review.]
    
    * CI: Resume testing on Intel macOS (#967)
    
    ---------
    
    Co-authored-by: Dilum Aluthge <dilum@aluthge.com>

--- src/PackageCompiler.jl
+++ src/PackageCompiler.jl
@@ -225,7 +225,11 @@ function create_fresh_base_sysimage(stdlibs::Vector{String}; cpu_target::String,
     sysimg_source_path = Base.find_source_file("sysimg.jl")
     base_dir = dirname(sysimg_source_path)
     tmp_corecompiler_ji = joinpath(tmp, "corecompiler.ji")
-    tmp_sys_o = joinpath(tmp, "sys.o")
+    tmp_sys_o = joinpath(tmp, "sys-o.a")
+    # This naming convention (`sys-o.a`) is necessary to make the sysimage
+    # work on macOS.
+    # Bug report: https://github.com/JuliaLang/PackageCompiler.jl/issues/738
+    # PR: https://github.com/JuliaLang/PackageCompiler.jl/pull/930
     tmp_sys_sl = joinpath(tmp, "sys." * Libdl.dlext)
 
     compiler_source_path = joinpath(base_dir, "compiler", "compiler.jl")
@@ -639,7 +643,11 @@ function create_sysimage(packages::Union{Nothing, Symbol, Vector{String}, Vector
     end
 
     # Create the sysimage
-    object_file = tempname() * ".o"
+    object_file = tempname() * "-o.a"
+    # This naming convention (`-o.a`) is necessary to make the sysimage
+    # work on macOS.
+    # Bug report: https://github.com/JuliaLang/PackageCompiler.jl/issues/738
+    # PR: https://github.com/JuliaLang/PackageCompiler.jl/pull/930
 
     create_sysimg_object_file(object_file, packages, packages_sysimg;
                             project,
@@ -1428,7 +1436,7 @@ function bundle_julia_libexec(ctx, dest_dir)
     p7zip_exe = basename(p7zip_path)
     cp(p7zip_path, joinpath(bundle_libexec_dir, p7zip_exe))
 
-    return 
+    return
 end
 
 function recursive_dir_size(path)
